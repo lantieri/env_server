233657c23e39e0230cd9a8fc39232eb7e3940fb5
lantieri <62449736+lantieri@users.noreply.github.com>
2024-03-18 22:28:30 +0100

    Add files via upload

--- DonneesTP.js
+++ DonneesTP.js
@@ -13,29 +13,24 @@ $(document).ready(function(){
         $("#sujet").show();
 
 
-        $("#valEx1").html(x0-10);
-        $("#valEx1b").html(x1+5);
+        $("#valEx1").html(x0/10);
+        $("#valEx1b").html(x1);
         
         $("#valEx2").html(x0);
-        $("#valEx2b").html(x0);
-        $("#valEx2c").html(x0);
-        $("#valEx2d").html(x0);
-        $("#valEx2e").html(x1);
-        $("#valEx2f").html(x1);
-        $("#valEx2g").html(x1);
-        $("#valEx2h").html(x1);
+        $("#valEx2b").html(x1);
 
-        $("#valEx3").html(x0);
-        $("#valEx3b").html(x1);
+        $("#valEx3").html(x0+3*x1);
 
         $("#valEx4").html(x0);
         $("#valEx4b").html(x1);
+        $("#valEx4c").html(x0);
+        $("#valEx4d").html(x1);
         
         $("#valEx5").html(x1/10);
         $("#valEx5b").html(x0);
 
         $("#valEx6").html(x1);
-        $("#valEx6b").html(Math.abs(x0-x1))
+        $("#valEx6b").html(x0-x1)
         $("#valEx6c").html(x0/(100+x1));
         $("#valEx6d").html(x0/(100+x1));
     });
