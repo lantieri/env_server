99952c56484f85be6839225f7c7b9d9d3cc0890b
lantieri <62449736+lantieri@users.noreply.github.com>
2025-01-20 20:01:39 +0100

    Add files via upload

--- DonneesTP.js
+++ DonneesTP.js
@@ -13,25 +13,34 @@ $(document).ready(function(){
         $("#sujet").show();
 
 
-        $("#valEx1").html(x0/10);
-        $("#valEx1b").html(x1);
+        $("#valEx1").html(x0);
         
-        $("#valEx2").html(x0);
-        $("#valEx2b").html(x1);
+        $("#valEx2").html(x1);
 
-        $("#valEx3").html(x0+3*x1);
+        $("#valEx3").html(x2);
 
         $("#valEx4").html(x0);
         $("#valEx4b").html(x1);
         $("#valEx4c").html(x0);
-        $("#valEx4d").html(x1);
-        
-        $("#valEx5").html(x1/10);
-        $("#valEx5b").html(x0);
+        $("#valEx4d").html(x0);
+        $("#valEx4e").html(x0);
+        $("#valEx4f").html(x0);
 
-        $("#valEx6").html(x1);
-        $("#valEx6b").html(x0-x1)
-        $("#valEx6c").html(x0/(100+x1));
-        $("#valEx6d").html(x0/(100+x1));
+        $("#valEx5").html(x0);
+        $("#valEx5b").html(2*x1);
+
+        $("#valEx6").html(7+x0);
+        $("#valEx6").html(7+x0);
+        $("#valEx6b").html(x1/2);
+        $("#valEx6c").html((x0-x1)/5);
+
+        $("#valEx7").html(x0);
+        $("#valEx7b").html(x1);
+
+        $("#valEx8").html(x0);
+        $("#valEx8b").html(x1);
+        $("#valEx8c").html(x1+0.5);
+        $("#valEx8d").html(6*x0);
+        $("#valEx8e").html(5/(2*x0));
     });
 });
